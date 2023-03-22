49d6fd1040bcde0b002578840e94769a00bfbb6c
Syed-Nasir <92010892+Syed-Nasir@users.noreply.github.com>
2023-03-22 18:03:22 +0530

    Add files via upload

---
+++ NASIR PORTFOLIO/script.js
@@ -0,0 +1,19 @@
+console.log("script running")
+document.querySelector('.cross').style.display = 'none'
+document.querySelector('.hamburger').addEventListener("click", () => {
+    document.querySelector('.sidebar').classList.toggle('sidebargo')
+    if (document.querySelector('.sidebar').classList('sidebargo')) {
+        document.querySelector('.ham').style.display = 'inline'
+        document.querySelector('.cross').style.display = 'none'
+
+    }
+    else {
+        document.querySelector('.cross').style.display = 'inline'
+        setTimeout(() => {
+            document.querySelector('.ham').style.display = 'none'
+
+        }, 300);
+
+
+    }
+})
